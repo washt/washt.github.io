abea05958e302cea9fb7ea8d36c543b5988b0b5a
Tucker Wash <washt23@gmail.com>
2016-07-06 00:57:34 -0400

    mvp landing

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import Radium from 'radium';
+import fa from 'react-fontawesome';
 
 @Radium
 export default class Landing extends React.Component {
   render() {
     return (
       <div className="Landing" style={[landing.div]}>
-				<h2>Helllo World</h2>
+        <h2 style={[landing.brand]}>Tucker Wash</h2>
+	<p style={[landing.message]}>
+	Polyglot Programmer interested in startups, AI, and The Web.</p>
+	<div className="Links" style={[landing.brand]}>
+	  <a style={[landing.link,
+		     landing.yellow]} 
+		   href="https://www.github.com/washt"> Portfolio </a>
+	  <a style={[landing.link,
+		     landing.green]}
+		   href="https://www.github.com/washt">
+		   <fa className="fa fa-github"></fa> Github </a>
+	  <a style={[landing.link,
+		     landing.blue]}
+		   href="https://www.twitter.com/ducktuckgo">
+		   <fa className="fa fa-twitter"></fa> Twitter </a>
+	  <a style={[landing.link,
+		     landing.red]}
+		   href="https://www.keybase.io/tucker">
+		   <fa className="fa fa-key"></fa> Keybase </a>
+        </div>
       </div>
      )
   }
@@ -14,6 +34,31 @@ export default class Landing extends React.Component {
 
 var landing = {
   div: {
-    background: "#00FF88",
+    marginTop: "20%",
+    background: "#363636",
+    color: "#FFFFFF",
+    fontFamily: "Source Code Pro",
+  },
+  brand: {
+    textAlign: "center",
+  },
+  message: {
+    textAlign: "center",
+  },
+  link: {
+    textDecoration: "none",
+  },
+  green: {
+    color: "#45F297",
+  },
+  blue: {
+    color: "#179FE5",
+  },
+  red: {
+    color: "#F25645",
+  },
+  yellow: {
+    color: "#E3EB1F",
   }
+
 }
